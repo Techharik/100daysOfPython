ebbda480387a58919bb84b1ba7ed909295129180
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-07-03 08:59:20 +0000

    Completed Day1 of python . Created the Band genrator

--- Day1.py
+++ Day1.py
@@ -1,1 +1,22 @@
-print('Hello')
+# print('Day1 Python')
+# print('The Funcion is printed like this')
+# print('print("what is print")')
+
+# Hello world
+# input('What is your name?')
+
+# print( len(input('What is Your name?')))
+
+# name = input('What is Your Name?')
+# print(name)
+
+''' projecct day1 '''
+
+#Band Generator App: Combination of city and print
+
+city = input('What is your city Name?')
+pet = input ('What is you pet Name?')
+
+Band = city +' '+ pet
+
+print('Your BandName is '+ Band)
