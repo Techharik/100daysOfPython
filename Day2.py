673d609148c2c842474166ea47b58bf66e02a3d1
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-07-06 07:56:25 +0000

    Day3 statered conditional statement, logical operator, code blocks and scope

--- Day2.py
+++ Day2.py
@@ -2,37 +2,37 @@
 
 # Type casting or type converstion Practice !
 
-# num = input('What is the two digit number?')
+num = input('What is the two digit number?')
 
-# str_num = str(num)
+str_num = str(num)
 
-# num_1 = int(str_num[0]) + int(str_num[1])
+num_1 = int(str_num[0]) + int(str_num[1])
 
-# print(num_1)
+print(num_1)
 
 
 # BMI Calculator;
 
-# weight = int(input('What is your weight?'))
-# height = float(input('What is your height?'))
+weight = int(input('What is your weight?'))
+height = float(input('What is your height?'))
 
-# Bmi = weight / (height**2)
+Bmi = weight / (height**2)
 
-# print(int(Bmi))
+print(int(Bmi))
 
 # life week left - 90
 
-# myAge = int(input('What is your age?'))
+myAge = int(input('What is your age?'))
 
-# year_left = 90 - myAge;
+year_left = 90 - myAge;
 
-# month_left = year_left*12
+month_left = year_left*12
 
-# week_left = year_left*52
+week_left = year_left*52
 
-# days_left =year_left*365
+days_left =year_left*365
 
-# print(f'you have {days_left}days , {week_left}weeks , {month_left} months left  ')
+print(f'you have {days_left}days , {week_left}weeks , {month_left} months left  ')
 
 
 # tip Calculator
---
+++ Day3.py
@@ -0,0 +1,11 @@
+# print('Welcome to Day-3')
+print('Conditional statement , logical Operator , code blocks and Scope')
+
+#odd or evan calculator:
+
+number = int(input('What is the number?'))
+
+if number%2 == 0:
+    print('The given number is Even')
+else:
+    print('The Given number is Odd')
