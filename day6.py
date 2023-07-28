d65d613e26f5ec6d93a6dd380adbdc2baf0e75ab
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-07-28 06:35:12 +0000

    Day-6 is completed learned functions while, not keywords,

---
+++ day6.py
@@ -0,0 +1,22 @@
+''' Code Blocks , functions , While Loops '''
+
+
+# Functions:
+# def my_funtion():
+#     print('Hello world')
+
+# #While Loops:
+
+# i=0
+# while i<5:
+#     print('True')
+#     i+=1
+
+# #Not Keywork:
+
+''' The return value will be True if the statement(s) are 
+not True, otherwise it will return False. '''
+
+i = True
+
+print(not i)
