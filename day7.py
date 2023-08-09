dca2e3739b6741eb0bcfd130007c10be0ee734b2
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-08-09 14:11:31 +0000

    Created  a Hangman Project learned about loops not in if and nested if

--- day7.py
+++ day7.py
@@ -8,20 +8,35 @@ word_list = ['ardvark', 'babbon', 'Camel']
 random_number = random.randint(0, (len(word_list)-1))
 # we can use random.choice()- to get a random words in the list
 
-choose_word = word_list[random_number]
+choose_word = word_list[random_number].lower()
 
 print(choose_word)
-
-guess_letter = input('Guess Letter: ')
-
 game_word = []
-i = 0
+for _ in range(len(choose_word)):
+    game_word += '_'
 
-while (i < len(choose_word)):
-    if (guess_letter == choose_word[i].lower()):
-        game_word.append(guess_letter)
-    else:
-        game_word.append('_')
-    i += 1
 
-print(game_word)
+end_of_game = False
+level = len(choose_word)
+
+while not end_of_game:
+    guess_letter = input('Guess Letter: ').lower()
+    for pos in range(len(choose_word)):
+        letter = choose_word[pos]
+        if letter == guess_letter:
+            if game_word[pos] == '_':
+               game_word[pos] = letter
+            else:
+                print('letter already exist')
+        
+    print(game_word)
+
+    if guess_letter not in choose_word:
+        level -= 1
+        if level == 0:
+            print('You Lose')
+            end_of_game = True
+
+    if "_" not in game_word:
+        end_of_game = True
+        print('You won')
