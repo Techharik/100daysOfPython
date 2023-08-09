fd024faf6af021601b789b9dfbbe114104a0ca0e
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-08-09 14:33:00 +0000

    day 8 is started about function and inputs

---
+++ day8.py
@@ -0,0 +1,8 @@
+''' more abou functions arguments and  parameters '''
+
+def welcome(name,msg):
+    print(f"Welcome {name} {msg}")
+
+welcome('Siva','Hi')
+
+# name is the parameter and arugument given here
