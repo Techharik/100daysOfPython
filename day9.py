30efbbf88b424a9f43644449dfdab53ca42c15df
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-08-10 05:10:55 +0000

    Started day9 with dictionary and nesting

---
+++ day9.py
@@ -0,0 +1,11 @@
+''' dictionary and nesting '''
+
+dictionary = object 
+
+Person = {
+    'name' : 'Hari Prasath',
+    'Age' : 22
+}
+
+print(Person)
+print(Person['name']) #assging the data in the dictionary
