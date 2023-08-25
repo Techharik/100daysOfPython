04fc9f0f8f921ff3c40046620edc9d147412e717
Hari Prasath K <130031783+Techharik@users.noreply.github.com>
2023-08-25 10:14:24 +0000

    Debuuging Day-13 completed

--- day13.py
+++ day13.py
@@ -1,1 +1,35 @@
 ''' Debugging '''
+
+# Tips and techiniques.
+
+
+
+import random
+
+# descibe the problem:!
+def my_function():
+    # -Range Goes exclusive of 20 change the 20 to 21
+    for i in range(1, 21):
+        if i == 20:
+            print('You got it')
+
+
+my_function()
+
+# Reproduce the bug - Reading the error message and produce the error and find !
+# the bug.abs
+
+# Play reading to the computer - read your code like an computer human.!
+
+# fix the error - By googling the related error like stack overflow.!
+
+# print is your friend - like console for javascript to debugging.
+
+# use debugger tools - example - pythontutor.comp web debugger.
+
+# Final debuggeing - take a Nap , Ask a Friends (they have a different thinking)
+#  run your code often(see line by lien better ConnectionAbortedError)
+
+
+
+
